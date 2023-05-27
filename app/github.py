e00e7f3d37a3971cee3c468fab9bdab5a2d00f6b
Robinson Santillan <robinsonsantillan@gmail.com>
2023-05-27 10:43:26 -0500

    Compilacion 01

---
+++ app/github.py
@@ -0,0 +1,13 @@
+import requests
+
+class GitHubProfile:
+    
+    def __init__(self):
+        self.url_base = 'https://api.github.com/users/xxRobinsonxx'
+        data_perfil = requests.get(self.url_base).json()
+        self.nombre = data_perfil['name']
+        self.biografia = data_perfil['bio']
+        self.imagen = data_perfil['avatar_url']
+        self.ubicacion = data_perfil['location']
+        self.twitter = data_perfil['twitter_username']
+        self.github = data_perfil['html_url']
